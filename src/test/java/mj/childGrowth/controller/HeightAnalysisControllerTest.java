f7abe395fc3199b211a5d61eb2e0e4d3e0485592
MJbae <bmj5208@gmail.com>
2022-08-04 17:21:51 +0900

    Add test for HeightAnalysisController

---
+++ src/test/java/mj/childGrowth/controller/HeightAnalysisControllerTest.java
@@ -0,0 +1,33 @@
+package mj.childGrowth.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@WebMvcTest(HeightAnalysisController.class)
+@DisplayName("HeightAnalysisController")
+class HeightAnalysisControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Nested
+    @DisplayName("range 메소드는")
+    class Describe_range {
+
+        @Test
+        @DisplayName("HTTP Status Code 200 OK 응답한다")
+        void it_responds_with_200_ok() throws Exception {
+            mockMvc.perform(get("/height/range"))
+                    .andExpect(status().isOk());
+        }
+    }
+}
