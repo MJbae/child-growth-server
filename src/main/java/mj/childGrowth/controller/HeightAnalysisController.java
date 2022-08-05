9aae84ef0c8dc1fd8ec0981ec004f2a965cda408
MJbae <bmj5208@gmail.com>
2022-08-05 17:12:15 +0900

    Add Request params for HeightAnalysisController

--- src/main/java/mj/childGrowth/controller/HeightAnalysisController.java
+++ src/main/java/mj/childGrowth/controller/HeightAnalysisController.java
@@ -3,6 +3,7 @@ package mj.childGrowth.controller;
 import mj.childGrowth.controller.dto.AnalysisResponseData;
 import mj.childGrowth.controller.dto.HeightResponseData;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -12,10 +13,11 @@ import java.util.List;
 public class HeightAnalysisController {
 
     @RequestMapping("/height/range")
-    public AnalysisResponseData range() {
+    public AnalysisResponseData range(@RequestParam Integer year, @RequestParam Integer month,
+                                      @RequestParam Integer day, @RequestParam Float height,
+                                      @RequestParam String sex) {
 
         Integer RANGE_INDEX = 1;
-
         List<HeightResponseData> range = new ArrayList<>();
 
         return new AnalysisResponseData(RANGE_INDEX, range);
