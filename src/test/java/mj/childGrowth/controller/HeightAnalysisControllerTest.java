1dddf7c04fae49704f207a5cc15ebd7fca6022a2
MJbae <bmj5208@gmail.com>
2022-08-08 13:36:49 +0900

    Update HeightAnalysisControllerTest with Mocking Repository

--- src/test/java/mj/childGrowth/controller/HeightAnalysisControllerTest.java
+++ src/test/java/mj/childGrowth/controller/HeightAnalysisControllerTest.java
@@ -1,13 +1,20 @@
 package mj.childGrowth.controller;
 
+import mj.childGrowth.domain.HeightAnalysis;
+import mj.childGrowth.domain.HeightAnalysisRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -19,9 +26,20 @@ class HeightAnalysisControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private HeightAnalysisRepository repository;
+
+    private HeightAnalysis analysis;
+
     @Nested
     @DisplayName("range 메소드는")
     class Describe_range {
+        @BeforeEach
+        void setUp() {
+//            analysis = new HeightAnalysis(1, "male", 1, 10, 173);
+            given(repository.findAllByYearAndMonthAndDayAndSexAndHeight(1991, 3, 26, (float) 179.5, "male"))
+                    .willReturn(List.of());
+        }
 
         @Test
         @DisplayName("HTTP Status Code 200 OK 응답한다")
