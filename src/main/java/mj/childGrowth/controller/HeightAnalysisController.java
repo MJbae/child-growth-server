9bface27731a62a5a045f0e26f8cdfe830c1faae
MJbae <bmj5208@gmail.com>
2022-08-08 13:38:25 +0900

    Update HeightAnalysisController Treating persistent data from repository

--- src/main/java/mj/childGrowth/controller/HeightAnalysisController.java
+++ src/main/java/mj/childGrowth/controller/HeightAnalysisController.java
@@ -2,15 +2,21 @@ package mj.childGrowth.controller;
 
 import mj.childGrowth.controller.dto.AnalysisResponseData;
 import mj.childGrowth.controller.dto.HeightResponseData;
+import mj.childGrowth.domain.HeightAnalysisRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class HeightAnalysisController {
+    public final HeightAnalysisRepository repository;
+
+    public HeightAnalysisController(HeightAnalysisRepository repository) {
+        this.repository = repository;
+    }
 
     @RequestMapping("/height/range")
     public AnalysisResponseData range(@RequestParam Integer year, @RequestParam Integer month,
@@ -18,7 +24,9 @@ public class HeightAnalysisController {
                                       @RequestParam String sex) {
 
         Integer RANGE_INDEX = 1;
-        List<HeightResponseData> range = new ArrayList<>();
+        List<HeightResponseData> range = repository.findAllByYearAndMonthAndDayAndSexAndHeight(year, month, day, height, sex)
+                .stream().map(heightAnalysis -> new HeightResponseData(heightAnalysis.getPercentile(), heightAnalysis.getHeight()))
+                .collect(Collectors.toList());
 
         return new AnalysisResponseData(RANGE_INDEX, range);
     }
