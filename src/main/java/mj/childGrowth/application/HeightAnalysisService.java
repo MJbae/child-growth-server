a29b36622bb9be23aa615990680c392980e0dec6
MJbae <bmj5208@gmail.com>
2022-08-09 13:16:50 +0900

    Define HeightAnalysisService by migrating business logic from controller

---
+++ src/main/java/mj/childGrowth/application/HeightAnalysisService.java
@@ -0,0 +1,38 @@
+package mj.childGrowth.application;
+
+import mj.childGrowth.controller.dto.HeightResponseData;
+import mj.childGrowth.domain.HeightAnalysisRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class HeightAnalysisService {
+    public final HeightAnalysisRepository repository;
+
+    public HeightAnalysisService(HeightAnalysisRepository repository) {
+        this.repository = repository;
+    }
+
+    public int getRangeIndex(Float height, List<HeightResponseData> range) {
+        int rangeIndex = 0;
+        for (HeightResponseData analysis : range) {
+            if (analysis.getHeight() > height) {
+                break;
+            }
+            rangeIndex++;
+        }
+        return rangeIndex;
+    }
+
+    public List<HeightResponseData> showAllBy(Integer monthAfterBirth, Float height, String sex) {
+        return repository.findAllByMonthAndSexAndHeight(monthAfterBirth, height, sex)
+                .stream().map(heightAnalysis -> new HeightResponseData(heightAnalysis.getPercentile(), heightAnalysis.getHeight()))
+                .sorted(Comparator.comparingInt(HeightResponseData::getPercentile))
+                .collect(Collectors.toList());
+    }
+}
