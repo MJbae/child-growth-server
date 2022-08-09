0d0886330b758032b2aa15b724ed96fdf9f728bc
MJbae <bmj5208@gmail.com>
2022-08-09 13:17:21 +0900

    Remove business logic in controller by migrating those to service component

--- src/main/java/mj/childGrowth/controller/HeightAnalysisController.java
+++ src/main/java/mj/childGrowth/controller/HeightAnalysisController.java
@@ -1,32 +1,32 @@
 package mj.childGrowth.controller;
 
+import mj.childGrowth.application.HeightAnalysisService;
 import mj.childGrowth.controller.dto.AnalysisResponseData;
 import mj.childGrowth.controller.dto.HeightResponseData;
-import mj.childGrowth.domain.HeightAnalysisRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 public class HeightAnalysisController {
-    public final HeightAnalysisRepository repository;
+    public final HeightAnalysisService service;
 
-    public HeightAnalysisController(HeightAnalysisRepository repository) {
-        this.repository = repository;
+    public HeightAnalysisController(HeightAnalysisService service) {
+        this.service = service;
     }
 
     @RequestMapping("/height/range")
     public AnalysisResponseData range(@RequestParam Integer monthAfterBirth, @RequestParam Float height,
                                       @RequestParam String sex) {
 
-        Integer RANGE_INDEX = 1;
-        List<HeightResponseData> range = repository.findAllByMonthAndSexAndHeight(monthAfterBirth, height, sex)
-                .stream().map(heightAnalysis -> new HeightResponseData(heightAnalysis.getPercentile(), heightAnalysis.getHeight()))
-                .collect(Collectors.toList());
+        List<HeightResponseData> range = service.showAllBy(monthAfterBirth, height, sex);
+        int rangeIndex = service.getRangeIndex(height, range);
 
-        return new AnalysisResponseData(RANGE_INDEX, range);
+
+        return new AnalysisResponseData(rangeIndex, range);
     }
+
+
 }
