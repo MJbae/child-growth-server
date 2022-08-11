94bb2276b31e6b0a56469e61c08cfc9589d79e42
MJbae <bmj5208@gmail.com>
2022-08-11 22:50:43 +0900

    Add SQL script for initializing data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into height_analysis (id, percentile, height, `month`, sex) values (1, 10, 167.5, 227, 'male');
+insert into height_analysis (id, percentile, height, `month`, sex) values (2, 25, 170.8, 227, 'male');
+insert into height_analysis (id, percentile, height, `month`, sex) values (3, 50, 174.5, 227, 'male');
+insert into height_analysis (id, percentile, height, `month`, sex) values (4, 75, 180.4, 227, 'male');
+insert into height_analysis (id, percentile, height, `month`, sex) values (5, 90, 183.9, 227, 'male');
+
+
+
+
