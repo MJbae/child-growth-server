6022d4a4e9a33716037bca0f1090bbcf8d8969c7
MJbae <bmj5208@gmail.com>
2022-09-03 20:52:33 +0900

    Add Filter for measuring api speed

---
+++ src/main/java/mj/childGrowth/config/SpeedMeasurementFilter.java
@@ -0,0 +1,38 @@
+package mj.childGrowth.config;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.AbstractRequestLoggingFilter;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Configuration
+public class SpeedMeasurementFilter extends AbstractRequestLoggingFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+    final String REQUESTED_AT = "requestedAt";
+    final String SLOW_TAG = "SLOW_API: ";
+
+    @Override
+    protected void beforeRequest(HttpServletRequest request, String message) {
+        request.setAttribute(REQUESTED_AT, System.currentTimeMillis());
+        logger.debug(message);
+    }
+
+    @Override
+    protected void afterRequest(HttpServletRequest request, String message) {
+        Object requestedAt = request.getAttribute(REQUESTED_AT);
+
+        if (requestedAt == null) {
+            logger.debug(message);
+            return;
+        }
+
+        double elapsed = (System.currentTimeMillis() - (long) requestedAt) / 1000.0;
+        String prefixTag = (elapsed > 1) ? SLOW_TAG : "";
+
+        logger.info(prefixTag + " " + message + " elapsed=" + elapsed);
+    }
+}
