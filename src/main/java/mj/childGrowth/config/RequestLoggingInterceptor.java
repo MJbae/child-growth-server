5f8b093f13e605c758d868ccb5c7745824c6b7f8
MJbae <bmj5208@gmail.com>
2022-09-04 16:30:41 +0900

    Refactor Config codes by integrating into WebConfig

--- src/main/java/mj/childGrowth/config/RequestLoggingInterceptor.java
+++ src/main/java/mj/childGrowth/config/RequestLoggingInterceptor.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
--- src/main/java/mj/childGrowth/config/WebConfig.java
+++ src/main/java/mj/childGrowth/config/WebConfig.java
@@ -1,19 +1,32 @@
 package mj.childGrowth.config;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
-
-    private final RequestLoggingInterceptor interceptor;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(interceptor)
+        registry.addInterceptor(new RequestLoggingInterceptor())
                 .addPathPatterns("/api/height/**");
+    }
+
+    @Bean
+    public FilterRegistrationBean setSpeedMeasurementFilterRegistration() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new SpeedMeasuringFilter());
+        filterRegistrationBean.setOrder(1);
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean setRequestLoggingFilterRegistration() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(new RequestLoggingFilter());
+        filterRegistrationBean.addUrlPatterns("/api/height/*");
+        filterRegistrationBean.setOrder(2);
+        return filterRegistrationBean;
     }
 }
