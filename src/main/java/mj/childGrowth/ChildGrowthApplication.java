a7335a1721774e320bf2c2f88ff1d8342754b168
MJbae <bmj5208@gmail.com>
2022-09-15 18:06:22 +0900

    Add Scheduler Settings

--- src/main/java/mj/childGrowth/ChildGrowthApplication.java
+++ src/main/java/mj/childGrowth/ChildGrowthApplication.java
@@ -3,7 +3,9 @@ package mj.childGrowth;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableBatchProcessing
 @SpringBootApplication
 public class ChildGrowthApplication {
