ed071edaf22e2a77fce9248c19b4fa72b5a1fcb7
MJbae <bmj5208@gmail.com>
2022-09-24 16:25:38 +0900

    Apply Request Param Validation on HeightAnalysisController

--- src/main/java/mj/childGrowth/controller/HeightAnalysisController.java
+++ src/main/java/mj/childGrowth/controller/HeightAnalysisController.java
@@ -3,7 +3,11 @@ package mj.childGrowth.controller;
 import mj.childGrowth.application.HeightAnalysisService;
 import mj.childGrowth.controller.dto.AnalysisResponseData;
 import mj.childGrowth.controller.dto.HeightResponseData;
+import mj.childGrowth.controller.validator.HeightValidation;
+import mj.childGrowth.controller.validator.MonthValidation;
+import mj.childGrowth.controller.validator.SexValidation;
 import mj.childGrowth.domain.Sex;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Validated
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class HeightAnalysisController {
@@ -21,8 +26,9 @@ public class HeightAnalysisController {
     }
 
     @RequestMapping("/api/height/range")
-    public AnalysisResponseData range(@RequestParam Integer monthAfterBirth, @RequestParam Float height,
-                                      @RequestParam String sex) {
+    public AnalysisResponseData range(@RequestParam @MonthValidation Integer monthAfterBirth,
+                                      @RequestParam @HeightValidation Float height,
+                                      @RequestParam @SexValidation String sex) {
 
         List<HeightResponseData> range = service.showAllBy(monthAfterBirth, height, Sex.valueOf(sex.toUpperCase()));
         int rangeIndex = service.getRangeIndex(height, range);
