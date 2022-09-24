0deae66d96dcd52287e30e23974a102f474caaf8
MJbae <bmj5208@gmail.com>
2022-09-24 16:25:10 +0900

    Define Month Validation on Request Param Month

---
+++ src/main/java/mj/childGrowth/controller/validator/MonthValidation.java
@@ -0,0 +1,17 @@
+package mj.childGrowth.controller.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = MonthValidator.class)
+@Target({ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MonthValidation {
+    String message() default "Invalid Month";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+}
