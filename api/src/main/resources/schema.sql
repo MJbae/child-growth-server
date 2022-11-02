7800c283019d72662e6704d4700aaa8be5c476c0
배만진 <111344711+mj-at-ogq@users.noreply.github.com>
2022-11-02 10:43:07 +0900

    Change schema.sql according to proper module

--- api/src/main/resources/schema.sql
+++ api/src/main/resources/schema.sql
@@ -19,39 +19,6 @@ CREATE TABLE IF NOT EXISTS `growth_analysis`.`height_analysis`
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
-CREATE SCHEMA IF NOT EXISTS `log` DEFAULT CHARACTER SET utf8mb4;
-USE `log`;
-
-DROP TABLE IF EXISTS `log`.`log_height_request`;
-
-CREATE TABLE IF NOT EXISTS `log`.`log_height_request`
-(
-    `id`          BIGINT     NOT NULL AUTO_INCREMENT,
-    `height`       FLOAT NOT NULL,
-    `month` INT(11) NULL     DEFAULT NULL,
-    `created_at`    DATETIME     NULL     DEFAULT NULL,
-    `sex`      VARCHAR(255)  NOT NULL DEFAULT 'MALE',
-    PRIMARY KEY (`id`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-DROP TABLE IF EXISTS `log`.`aggregation_height_request`;
-CREATE TABLE IF NOT EXISTS `log`.`aggregation_height_request`
-(
-    `id`          BIGINT     NOT NULL AUTO_INCREMENT,
-    `height_average`       FLOAT  NULL,
-    `month_average`       FLOAT  NULL,
-    `request_total_count` INT(11) NULL     DEFAULT NULL,
-    `male_count` INT(11) NULL     DEFAULT NULL,
-    `female_count` INT(11) NULL     DEFAULT NULL,
-    `updated_at`    DATETIME     NULL     DEFAULT NULL,
-    `created_at`    DATETIME     NULL     DEFAULT NULL,
-    PRIMARY KEY (`id`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
 SET SQL_MODE = @OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS = @OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS = @OLD_UNIQUE_CHECKS;
--- log/src/main/resources/schema.sql
+++ log/src/main/resources/schema.sql
@@ -2,26 +2,12 @@ SET @OLD_UNIQUE_CHECKS = @@UNIQUE_CHECKS, UNIQUE_CHECKS = 0;
 SET @OLD_FOREIGN_KEY_CHECKS = @@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS = 0;
 SET @OLD_SQL_MODE = @@SQL_MODE, SQL_MODE = 'TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `growth_analysis` DEFAULT CHARACTER SET utf8mb4;
-USE `growth_analysis`;
+CREATE SCHEMA IF NOT EXISTS `log` DEFAULT CHARACTER SET utf8mb4;
+USE `log`;
 
-DROP TABLE IF EXISTS `growth_analysis`.`height_analysis`;
+DROP TABLE IF EXISTS `log`.`log_height_request`;
 
-CREATE TABLE IF NOT EXISTS `growth_analysis`.`height_analysis`
-(
-    `id`          BIGINT     NOT NULL AUTO_INCREMENT,
-    `height`       FLOAT NOT NULL,
-    `month` INT(11) NULL     DEFAULT NULL,
-    `percentile`    INT(11)        NULL     DEFAULT NULL,
-    `sex`      VARCHAR(255)  NOT NULL DEFAULT 'MALE',
-    PRIMARY KEY (`id`)
-)
-ENGINE = InnoDB
-DEFAULT CHARACTER SET = utf8mb4;
-
-DROP TABLE IF EXISTS `growth_analysis`.`height_request_log`;
-
-CREATE TABLE IF NOT EXISTS `growth_analysis`.`height_request_log`
+CREATE TABLE IF NOT EXISTS `log`.`log_height_request`
 (
     `id`          BIGINT     NOT NULL AUTO_INCREMENT,
     `height`       FLOAT NOT NULL,
@@ -33,8 +19,8 @@ CREATE TABLE IF NOT EXISTS `growth_analysis`.`height_request_log`
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4;
 
-DROP TABLE IF EXISTS `growth_analysis`.`height_request_aggregation`;
-CREATE TABLE IF NOT EXISTS `growth_analysis`.`height_request_aggregation`
+DROP TABLE IF EXISTS `log`.`aggregation_height_request`;
+CREATE TABLE IF NOT EXISTS `log`.`aggregation_height_request`
 (
     `id`          BIGINT     NOT NULL AUTO_INCREMENT,
     `height_average`       FLOAT  NULL,
